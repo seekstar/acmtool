da60b10be366856561eadc6c667ec2d5997f7873
Jiansheng QIU <632863986@qq.com>
2021-11-18 11:04:23 +0800

    LCT

---
+++ data_structure/LCT.cpp
@@ -0,0 +1,135 @@
+struct LCT {
+	int c[MAXN][2], fa[MAXN], val[MAXN], s[MAXN], sta[MAXN];
+	bool r[MAXN];
+
+	inline int& ls(int rt) {
+		return c[rt][0];
+	}
+	inline int& rs(int rt) {
+		return c[rt][1];
+	}
+	inline bool not_splay_rt(int x) {
+		return ls(fa[x]) == x || rs(fa[x]) == x;
+	}
+	inline int side(int x) {
+		return x == rs(fa[x]);
+	}
+	// void Init() {
+		// Initially every node is a tree by itself.
+		// memset all to 0. 
+	// }
+	inline void pushup(int x) {
+		s[x] = s[ls(x)] ^ val[x] ^ s[rs(x)];
+	}
+	inline void pushr(int x) {
+		swap(ls(x), rs(x));
+		r[x] ^= 1;
+	}
+	inline void pushdown(int x) {
+		if (r[x]) {
+			if (ls(x))
+				pushr(ls(x));
+			if (rs(x))
+				pushr(rs(x));
+			r[x] = false;
+		}
+	}
+	// s[x] is not updated
+	void __rotate_up(int x) {
+		int y = fa[x], z = fa[y], side_x = side(x), w = c[x][side_x ^ 1];
+		fa[x] = z;
+		if (not_splay_rt(y))
+			c[z][side(y)] = x;
+		if (w)
+			fa[w] = y;
+		c[y][side_x] = w;
+		fa[y] = x;
+		c[x][side_x ^ 1] = y;
+		pushup(y);
+	}
+	// inline void rotate_up(int x) {
+	// 	__rotate_up(x);
+	// 	pushup(x);
+	// }
+	// s[x] is not updated
+	void __splay(int x) {
+		int y = x, top = 0;
+		while(1) {
+			sta[++top] = y;
+			if (!not_splay_rt(y))
+				break;
+			y = fa[y];
+		}
+		int to = fa[y];
+		while (top)
+			pushdown(sta[top--]);
+		while (fa[x] != to) {
+			int y = fa[x];
+			if (fa[y] != to)
+				__rotate_up(side(x) == side(y) ? y : x);
+			__rotate_up(x);
+		}
+	}
+	inline void splay(int x) {
+		__splay(x);
+		pushup(x);
+	}
+	void access(int x) {
+		int ori_x = x;
+		for (int w = 0; x; w = x, x = fa[x]) {
+			__splay(x);
+			rs(x) = w;
+			pushup(x);
+		}
+		splay(ori_x);
+	}
+	int find_root(int x) {
+		access(x);
+		for (; ls(x); x = ls(x))
+			pushdown(x);
+		splay(x);
+		return x;
+	}
+	inline void make_root(int x) {
+		access(x);
+		pushr(x);
+	}
+	// void link_new(int x, int y) {}
+	void link(int x, int y) {
+		make_root(x);
+		if (find_root(y) == x)
+			return;
+		// If simply fa[x] = y, the complexity might be wrong.
+		access(y);
+		pushdown(x);
+		fa[y] = x;
+		ls(x) = y;
+		pushup(x); // Might be unnecessary
+	}
+	// cut(x, parent(x))
+	// inline void cut(int x) {
+	// 	access(x);
+	// 	if (ls(x) == 0)
+	// 		return;	// x has no parent
+	// 	fa[ls(x)] = 0;
+	// 	ls(x) = 0;
+	// }
+	void cut(int x, int y) {
+		make_root(x);
+		access(y);
+		if (ls(y) != x || rs(x) != 0)
+			return;	// No such edge (x, y)
+		fa[x] = ls(y) = 0;
+		pushup(y); // Might be unnecessary
+	}
+	inline int path_sum(int x, int y) {
+		make_root(x);
+		access(y);
+		return s[y];
+	}
+	inline void update_val(int x, int v) {
+		__splay(x);
+		val[x] = v;
+		pushup(x); // Might be unnecessary.
+	}
+};
